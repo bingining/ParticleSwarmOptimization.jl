85dacda7e8d1c280806155814af4a0e97a762100
bingining <bingining@gmail.com>
2019-03-06 17:15:08 +0800

    upgrade to compatible with julia 1.1

--- src/Problem.jl
+++ src/Problem.jl
@@ -1,12 +1,13 @@
 using Knet
 
-abstract Data
-type Single <: Data
+abstract type Data end
+
+struct Single <: Data
     x
     y
 end
 
-type MiniBatch <: Data
+struct MiniBatch <: Data
     trn
     dev
     tst
@@ -14,8 +15,9 @@ type MiniBatch <: Data
     MiniBatch(trn, tst) = new(trn, Any[], tst)
 end
 
-abstract Problem
-type RegularProblem <: Problem
+abstract type Problem end
+
+struct RegularProblem <: Problem
     f::Function
     dim::Int
     l::Array{Float64, 1}
@@ -24,7 +26,7 @@ type RegularProblem <: Problem
     RegularProblem(f, dim, l::Array{Float64,1}, h::Array{Float64,1}) = new(f, dim, l, h)
 end
 
-type NNProblem <: Problem
+struct NNProblem <: Problem
     f::Symbol
     l::Float64
     h::Float64
